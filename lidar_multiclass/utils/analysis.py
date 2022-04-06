7b0e9a789b37186e39127cd0fd3b25868c43906b
CharlesGaydon <charles.gaydon@gmail.com>
2022-04-06 10:56:25 +0200

    Support output of mean entropy raster

--- lidar_multiclass/utils/analysis.py
+++ lidar_multiclass/utils/analysis.py
@@ -39,32 +39,47 @@ def run_pdal_info(las: utils.PathLike, out_metadata_file: utils.PathLike) -> Non
     )
 
 
-def calc_entropy_stats(las: utils.PathLike) -> Tuple[float, np.array]:
-    """Returns average entropy and deciles of entropy
+def calc_entropy_stats(
+    las: utils.PathLike, output_dir: utils.PathLike
+) -> Tuple[float, np.array]:
+    """Returns average entropy and deciles of entropy"""
 
-    Args:
-        las (utils.PathLike): _description_
-
-    Returns:
-        Tuple[float, np.array]: _description_
-    """
     pipeline = pdal.Pipeline() | pdal.Reader.las(filename=las)
     pipeline.execute()
     data = pipeline.arrays[0]
     e = data["entropy"]
     deciles_q = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    return np.average(e), np.quantile(e, q=deciles_q)
+
+    avg = np.average(e)
+    deciles = np.quantile(e, q=deciles_q)
+    data["entropy"] = avg
+
+    output_tif = os.path.join(output_dir, os.path.basename(las).replace(".las", ".tif"))
+
+    pipeline = pdal.Writer.gdal(
+        filename=output_tif,
+        dimension="entropy",
+        data_type="float",
+        output_type="mean",
+        window_size=0.5,
+        resolution=1,
+        nodata=0,
+    ).pipeline(data)
+    pipeline.execute()
+    return avg, deciles
 
 
 def summarize_entropies(
-    files: List[utils.PathLike], out_metadata_file: utils.PathLike
+    files: List[utils.PathLike], output_dir: utils.PathLike
 ) -> None:
 
     for las in files:
-        avg, deciles = calc_entropy_stats(las)
-        print(avg, deciles)
+        avg, deciles = calc_entropy_stats(las, output_dir)
+        print(las)
+        print(f"average: {avg}, deciles: {deciles}")
 
 
 # For now this analysis is hardoced. After delivery, clean and document everything in a specific script.
 files = glob.glob("/var/data/cgaydon/data/temp/batch_6_predicted/89300*.las")
-print(calc_entropy_stats(files))
+output_dir = "/home/cgaydon/repositories/custom_analyses/20220406_entropy_maps/outputs/"
+summarize_entropies(files, output_dir)
